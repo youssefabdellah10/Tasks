5724b5aaa54c064af2168565740706ca571975aa
alibedair <85792381+alibedair@users.noreply.github.com>
2025-05-15 21:27:25 +0300

    .

---
+++ Distributed_System/Assignment_2/Order/demo/src/main/java/com/example/demo/Models/Payment.java
@@ -0,0 +1,48 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String paymentId;
+    
+    private boolean paymentStatus;
+      @Column(name = "customer_username", nullable = false)
+    private String customerUsername;
+    
+    private String type;
+    
+    private double balance;
+    public Payment(){
+        this.paymentStatus = false;
+    }
+    public void setpaymentstatus(boolean paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public String getPaymentId() {
+        return paymentId;
+    }
+    public boolean getPaymentStatus() {
+        return paymentStatus;
+    }    public String getCustomerUsername() {
+        return customerUsername;
+    }
+    public String getType() {
+        return type;
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+}
---
+++ Distributed_System/Assignment_2/Order/demo/src/main/java/com/example/demo/Repositories/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Models.Payment;
+
+import jakarta.inject.Singleton;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Singleton
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+}

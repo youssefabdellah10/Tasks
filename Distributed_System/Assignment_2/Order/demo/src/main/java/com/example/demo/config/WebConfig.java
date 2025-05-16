52c1eed3028d69c326e745597d3fc187a01e6698
youssef abdellah <youssefabdellah10@outlook.com>
2025-05-16 23:50:28 +0300

    dafadfs

--- Distributed_System/Assignment_2/Order/demo/src/main/java/com/example/demo/config/WebConfig.java
+++ Distributed_System/Assignment_2/Order/demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Your React application's URL
+                        .allowedOrigins("http://localhost:3000") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
